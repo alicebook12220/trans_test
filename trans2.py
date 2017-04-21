ee349cea04adfd847ee1ca99ebf36e4faa0de0ad
alicebook12220 <alicebook12220@gmail.com>
2017-04-21 18:32:34 +0800

    Add files via upload

--- trans2.py
+++ trans2.py
@@ -7,18 +7,18 @@ from scipy.linalg import expm3, norm
 def M(axis, theta):
     return expm3(cross(eye(3), axis/norm(axis)*theta))
 
-yaxis, theta =  [0,1,0], math.pi*22.5/180
+yaxis, theta =  [0,1,0], math.pi*-22.5/180
 
 M1 = M(yaxis, theta)
 print('M1:', M1)
 #t=[0, 0, 300] #C  -1.0249031413081129e+02,    3.8761348456470546e+00, 3.6926921486321095e+02
 t=[ -1.0249031413081129e+02,    3.8761348456470546e+00, 3.6926921486321095e+02] #C 
-Rinv = np.asarray(M1).tolist() #R
-print('Rinv:', Rinv)
-tinv = -dot(Rinv,t) #-RC
+Rtheta = np.asarray(M1).tolist() #Rtheta
+print('Rtheta:', Rtheta)
+tinv = -dot(Rtheta,t) #-RC
 print('t:', tinv)
 
 #R3=[ -7.9334588075188262e-01, -2.0247727636742888e-02, 7.7139635830836897e-03]
 R=[ -7.9334588075188262e-01, -2.0247727636742888e-02, 7.7139635830836897e-03]
-Rnew = dot(Rinv,R)	
+Rnew = dot(Rtheta,R)	
 print('R:', Rnew)
